6cbb9142dd72915c45b7539809389bc7fb11c35b
Arthuchaut <arthur.audebrand@gmail.com>
2019-05-18 19:36:46 +0200

    Init repos

---
+++ src/lib/Slider.js
@@ -0,0 +1,86 @@
+'use strict'
+
+export default class Slider {
+    constructor(options = {}) {
+        this.backgroundColor = options.backgroundColor || '#dddddd88';
+        this.stickColor = options.stickColor || '#4455aa';
+        this.width = options.width || 10;
+        this._scrollables = document.querySelectorAll('[scrollable="true"]');
+
+        this._implement();
+    }
+
+    get backgroundColor() { return this._backgroundColor; }
+    set backgroundColor(value) { this._backgroundColor = value; }
+
+    get stickColor() { return this._stickColor; }
+    set stickColor(value) { this._stickColor = value; }
+
+    get width() { return this._width; }
+    set width(value) { this._width = value; }
+
+    get _scrollables() { return this.__scrollables; }
+    set _scrollables(objects) { this.__scrollables = objects; }
+
+    _implement() {
+        for (let scrollable of this._scrollables) {
+            let slider = this._createScrollbar();
+
+            this._initProperties(scrollable);
+            scrollable.appendChild(slider.scrollbar);
+
+            slider.stick.onmousedown = e => {
+                scrollable.style.userSelect = 'none';
+
+                window.onmousemove = e2 => {
+                    let scrollTo = e2.pageY - scrollable.offsetTop - e.layerY;
+
+                    if (scrollTo >= 0 && scrollTo <= scrollable.clientHeight - slider.stick.clientHeight)
+                        slider.stick.style.top = scrollTo + 'px';
+                };
+            };
+
+            window.onmouseup = e => {
+                scrollable.style.userSelect = 'initial';
+                window.onmousemove = null
+            };
+        }
+    }
+
+    _initProperties(scrollable) {
+        if (scrollable.style.position === '')
+            scrollable.style.position = 'relative';
+
+        scrollable.style.overflow = 'hidden';
+        scrollable.style.paddingRight = (Number(scrollable.style.paddingRight) + this.width * 2) + 'px';
+    }
+
+    _createScrollbar() {
+        let scrollbar = document.createElement('div');
+        let stick = document.createElement('div');
+        
+        scrollbar.style.position = 'absolute';
+        scrollbar.style.top = '0px';
+        scrollbar.style.right = this.width + 'px';
+        scrollbar.style.width = '1px';
+        scrollbar.style.height = '100%';
+        scrollbar.style.backgroundColor = this.backgroundColor;
+
+        stick.style.position = 'absolute';
+        stick.style.top = '0px'; 
+        stick.style.left = -(this.width / 2) + 'px';
+        stick.style.width = this.width + 'px';
+        stick.style.height = (this.width * 2) + 'px';
+        stick.style.backgroundColor = this.stickColor;
+        stick.style.boxShadow = '1px 1px 5px #00000055';
+        stick.style.borderRadius = this.width + 'px';
+        stick.style.cursor = 'pointer';
+
+        scrollbar.appendChild(stick);
+
+        return {
+            scrollbar: scrollbar,
+            stick: stick
+        };
+    }
+}
